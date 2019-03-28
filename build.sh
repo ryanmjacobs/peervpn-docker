c108ac2c99a84f03fe7f74b374b0601516732736
Ryan Jacobs <ryan@rmj.us>
2019-03-27 17:53:43 -0700

    fix docker build path

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 version=1.3
 
 pushd client
-docker build -t ryanmjacobs/peervpn-client:1.4
+docker build -t ryanmjacobs/peervpn-client:1.4 .
 popd
 
 pushd server
-docker build -t ryanmjacobs/peervpn-server:1.4
+docker build -t ryanmjacobs/peervpn-server:1.4 .
 popd
