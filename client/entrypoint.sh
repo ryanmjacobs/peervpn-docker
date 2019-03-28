7a310f0d157f211f87122d8574ce7e8b96f03e40
Ryan Jacobs <ryan@rmj.us>
2019-03-27 17:08:30 -0700

    client/server mode

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # fallback static ip
-# will be overwritten quickly by the dhcp client
+# should be overwritten quickly by the dhcp client
 echo "ifconfig4 10.10.0.$((RANDOM % 251))/24" >> /etc/peervpn.conf
+echo "upcmd udhcpc -i peervpn0" >> /etc/peervpn.conf
 
 # launch peervpn
 peervpn /etc/peervpn.conf
