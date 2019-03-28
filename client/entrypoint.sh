0d5b41cc9a3f4018f3e58160475d1017a2c0571d
Ryan Jacobs <ryan@rmj.us>
2019-03-27 18:55:41 -0700

    fix fork

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -9,7 +9,7 @@ echo "ifconfig4 ${static_ip}/23" >> /tmp/peervpn.conf
 # WARNING: this timeout command is stupid; future OS versions deprecated
 #          the dash -t and this will throw an error. Busybox 1.29 -> 1.30
 #          breaks this
-echo "upcmd timeout -t 25 udhcpc -i peervpn0" >> /tmp/peervpn.conf
+echo "upcmd timeout -t 25 udhcpc -i peervpn0 &" >> /tmp/peervpn.conf
 
 # launch peervpn
 peervpn /tmp/peervpn.conf
