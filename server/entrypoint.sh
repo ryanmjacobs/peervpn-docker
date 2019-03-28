bf10269826f6a94079b12175a78bc7115ab99444
Ryan Jacobs <ryan@rmj.us>
2019-03-27 19:38:05 -0700

    agh wrong netmask :(

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,7 +4,7 @@ cp /{etc,tmp}/peervpn.conf
 
 # dhcp server node uses a static ip
 echo "ifconfig4 10.10.0.1/23" >> /tmp/peervpn.conf
-echo "upcmd dnsmasq -i peervpn0 --dhcp-range=10.10.0.10,10.10.0.250,255.255.255.0,12h" >> /tmp/peervpn.conf
+echo "upcmd dnsmasq -i peervpn0 --dhcp-range=10.10.0.10,10.10.0.250,255.255.254.0,12h" >> /tmp/peervpn.conf
 
 # launch peervpn
 peervpn /tmp/peervpn.conf
