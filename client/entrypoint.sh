4418c900af88460fc2b19a5df9af1ae966b183e4
Ryan Jacobs <ryan@rmj.us>
2019-03-27 19:24:21 -0700

    holy shit it works

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -3,13 +3,18 @@ cp /{etc,tmp}/peervpn.conf
 
 # generate static ip (for worst case if dhcp fails)
 static_ip="10.10.1.$((RANDOM % 251))"
-echo "ifconfig4 ${static_ip}/23" >> /tmp/peervpn.conf
+#echo "ifconfig4 ${static_ip}/23" >> /tmp/peervpn.conf
 
 # dhcp client
-# WARNING: this timeout command is stupid; future OS versions deprecated
-#          the dash -t and this will throw an error. Busybox 1.29 -> 1.30
-#          breaks this
-echo "upcmd timeout -t 25 udhcpc -i peervpn0 &" >> /tmp/peervpn.conf
+echo "upcmd dhclient -v -nw peervpn0" >> /tmp/peervpn.conf
+
+set_static() {
+    sleep 30
+    if ! ifconfig peervpn0 | grep "inet "; then
+        ifconfig peervpn0 $static_ip netmask 255.255.254.0
+    fi
+}
+set_static &
 
 # launch peervpn
 peervpn /tmp/peervpn.conf
