a1893cb86d2cf5c35b6e451bb18f522b04887cb2
Ryan Jacobs <ryan@rmj.us>
2019-03-27 18:14:00 -0700

    timeout for ifconfig set static

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-# fallback static ip
-# should be overwritten quickly by the dhcp client
-echo "ifconfig4 10.10.1.$((RANDOM % 251))/23" >> /etc/peervpn.conf
+# dhcp client
 echo "upcmd udhcpc -i peervpn0" >> /etc/peervpn.conf
+
+# set temp. static ip if cdhcp fails
+set_static() {
+    sleep 30
+
+    # ip address found!
+    ifconfig peervpn0 grep "inet " && return
+
+    static_ip="10.10.1.$((RANDOM % 251))/23"
+    ifconfig peervpn0 "$static_ip" netmask 255.255.254.0
+}
+set_static &
 
 # launch peervpn
 peervpn /etc/peervpn.conf
