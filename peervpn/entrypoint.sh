0ef8bdf948469cd4e6f4dbbcb52231379a2ad3df
Ryan Jacobs <ryan@rmj.us>
2019-03-29 03:59:19 -0700

    dont mess with hostname

--- peervpn/entrypoint.sh
+++ peervpn/entrypoint.sh
@@ -7,10 +7,5 @@ cp -v /{etc,tmp}/peervpn.conf
 static_ip="10.10.$((RANDOM%250)).$((RANDOM%250))"
 echo "ifconfig4 ${static_ip}/16" >> /tmp/peervpn.conf
 
-# modify hostname (external host can mount /etc/hostname to use this feature)
-hn=$(tr . - <<< $ip)
-echo $hn > /etc/hostname
-hostname $hn
-
 # launch peervpn
 peervpn /tmp/peervpn.conf
