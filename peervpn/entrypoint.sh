37f40a2e5d35df7968c532636f79e54303711472
Ryan Jacobs <ryan@rmj.us>
2019-03-29 02:41:16 -0700

    back to 10 10

--- peervpn/entrypoint.sh
+++ peervpn/entrypoint.sh
@@ -4,7 +4,7 @@
 cp -v /{etc,tmp}/peervpn.conf
 
 # generate static ip
-static_ip="10.01.$((RANDOM%250)).$((RANDOM%250))"
+static_ip="10.10.$((RANDOM%250)).$((RANDOM%250))"
 echo "ifconfig4 ${static_ip}/16" >> /tmp/peervpn.conf
 
 # modify hostname (external host can mount /etc/hostname to use this feature)
