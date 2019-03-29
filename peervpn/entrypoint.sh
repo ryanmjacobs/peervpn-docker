ff1d91f8ba55e1e71814cc9b5bfba127f079e660
core <core@rmj.us>
2019-03-29 11:20:14 +0000

    fu

--- peervpn/entrypoint.sh
+++ peervpn/entrypoint.sh
@@ -6,6 +6,7 @@ cp -v /{etc,tmp}/peervpn.conf
 # generate static ip
 static_ip="10.10.$((RANDOM%250)).$((RANDOM%250))"
 echo "ifconfig4 ${static_ip}/16" >> /tmp/peervpn.conf
+ifconfig peervpn0 ${static_ip}/16 # FUCK DOCKER
 
 # launch peervpn
 peervpn /tmp/peervpn.conf
